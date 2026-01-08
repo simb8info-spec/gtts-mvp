47458263614b30598ee3cc7e03e0eb4545b141a2
simb8info-spec <simb8.info@gmail.com>
2026-01-08 22:01:23 +0100

    Create navigator_home_page.dart

---
+++ lib/features/shell/presentation/navigator_home_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'map_page.dart';
+
+class NavigatorHomePage extends StatelessWidget {
+  const NavigatorHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // IMPORTANT: MapPage should preferably be content-only.
+    return const MapPage();
+  }
+}
